a5e8692c9a8be26ff0566c5f2dd67928daa67e7b
wangyanweiie <740484522@qq.com>
2021-09-18 10:26:54 +0800

    修改配置信息

--- src/main.js
+++ src/main.js
@@ -5,17 +5,14 @@ import store from './store'
 import toast from 'components/common/toast/index'
 import FastClick from 'fastclick'
 import VueLazyLoad from 'vue-lazyload'
+import './assets/css/base.css'
 import './assets/icon/iconfont.css'
 
-Vue.config.productionTip = false
-//添加事件总线对象
-Vue.prototype.$bus = new Vue()
-//安装toast插件
-Vue.use(toast)
-//调用attach方法解决移动端300ms延迟
-FastClick.attach(document.body)
-//使用图片懒加载插件 (将src改为v-lazy后在手机上不显示图片?)
-Vue.use(VueLazyLoad)
+Vue.prototype.$bus = new Vue()    //添加事件总线对象
+Vue.use(toast)                    //安装toast插件
+Vue.use(VueLazyLoad)              //使用图片懒加载插件 (将src改为v-lazy后在手机上不显示图片?)
+FastClick.attach(document.body)   //调用attach方法解决移动端300ms延迟
+Vue.config.productionTip = false  //阻止启动生产消息提示
 
 new Vue({
   router,
